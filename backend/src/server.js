c5e5ff89b5633d0498de469b0ae0f82cd267fc66
viqSTAR <prasadmanthan07@gmail.com>
2025-12-27 16:28:02 +0530

    start server method added

--- backend/src/server.js
+++ backend/src/server.js
@@ -26,7 +26,11 @@ if (ENV.NODE_ENV === 'production') {
 
 
 
-app.listen(ENV.PORT, () => {
-  console.log('Server is running on port 3000');
-  connectDB();
-});
+const startServer = async () => {
+  await connectDB();
+  app.listen(ENV.PORT, () => {
+    console.log(`Server is running on port ${ENV.PORT}`);
+  });
+}
+
+startServer();
