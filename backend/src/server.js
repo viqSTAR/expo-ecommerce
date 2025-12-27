aafdfb97a5decae070955611537491d3f073470c
viqSTAR <prasadmanthan07@gmail.com>
2025-12-27 11:13:35 +0530

    deployment config added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,26 @@
 import express from 'express';
+import path from 'path';
+import { ENV } from './config/env.js';
 
 const app = express();
+
+const __dirname = path.resolve();
 
 app.get('/api/health', (req, res) => {
     res.status(200).json({ message: 'Server is running' });
 });
 
-app.listen(3000, () => {
+// ready for deployment
+if (ENV.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../admin/dist')));
+    
+    app.get('/{*any}', (req, res) => {
+      res.sendFile(path.join(__dirname, '../admin/dist/index.html'));
+    });
+  }
+
+
+
+app.listen(ENV.PORT, () => {
   console.log('Server is running on port 3000');
 });
