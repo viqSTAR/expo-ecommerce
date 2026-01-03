b3d591ac6fb3372307f2c468e70fe3f837685b79
viqSTAR <prasadmanthan07@gmail.com>
2026-01-03 11:33:41 +0530

    feat: Add Inngest integration to synchronize user creation and deletion events with Clerk.

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -1,19 +1,19 @@
-import {Inngest} from 'inngest';
+import { Inngest } from 'inngest';
 import { connectDB } from './db.js';
 import { User } from '../models/user.model.js';
 
 export const inngest = new Inngest({ id: 'excommerce-app' });
 
 const syncUser = inngest.createFunction(
-    {id:'sync-user'},
-    {event:'clerk.user.created'},
-    async ({event}) => {
+    { id: 'sync-user' },
+    { event: 'clerk/user.created' },
+    async ({ event }) => {
         await connectDB();
-        const {id, email_address, first_name, last_name,image_url} = event.data;
+        const { id, email_addresses, first_name, last_name, image_url } = event.data;
 
         const newUser = {
             clerkId: id,
-            email: email_address[0]?.email_address,
+            email: email_addresses[0]?.email_address,
             name: `${first_name || ""} ${last_name || ""} || "User"`,
             imageUrl: image_url,
             addresses: [],
@@ -25,12 +25,12 @@ const syncUser = inngest.createFunction(
 );
 
 const deleteUserFromDB = inngest.createFunction(
-    {id:'delete-user-from-db'},
-    {event:'clerk.user.deleted'},
-    async ({event}) => {
+    { id: 'delete-user-from-db' },
+    { event: 'clerk/user.deleted' },
+    async ({ event }) => {
         await connectDB();
-        const {id} = event.data;
-        await User.deleteOne({clerkId: id});
+        const { id } = event.data;
+        await User.deleteOne({ clerkId: id });
     }
 );
 
